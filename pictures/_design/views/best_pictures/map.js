7e5654d64969204825ff4c3092f82b13615bdbe3
Marcin Rataj <m@lidel.org>
2010-04-12 22:51:18 +0200

    added todo

--- pictures/_design/views/best_pictures/map.js
+++ pictures/_design/views/best_pictures/map.js
@@ -1,6 +1,7 @@
 function(doc) {
   if (doc.doc_type == "CachedArtistPictures" && doc.lastfm) {
     var i=0;
+    // TODO: add logic that uses other present photo sources
     for(var i in doc.lastfm) {
       if (i==5) {
         break;
