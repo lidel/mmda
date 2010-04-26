71e50191103c05ec14362e2e874df3216778641c
Marcin Rataj <m@lidel.org>
2010-04-27 00:13:39 +0200

    fallback to search if mbid is not present

--- artists/urls.py
+++ artists/urls.py
@@ -6,3 +6,7 @@ urlpatterns = patterns('mmda.artists.views',
     url(r'^(?P<uri_artist>\S+)/(?P<mbid>\w{8}-\w{4}-\w{4}-\w{4}-\w{12})/$', 'show_artist', name='show-artist'),
     url(r'^(?P<uri_artist>\S+)/(?P<mbid>\w{8}-\w{4}-\w{4}-\w{4}-\w{12})/refresh/$', 'show_artist_refresh', name='show-artist-refresh'),
 )
+urlpatterns += patterns('mmda.search.views',
+   (r'^\S+/release/(?P<query_string>\S+)/$', 'create_search_result', {'query_type':'release'}),
+   (r'^(?P<query_string>\S+)/$', 'create_search_result', {'query_type':'artist'}),
+)
