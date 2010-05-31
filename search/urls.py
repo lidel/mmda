aa9cb0b13d2a8568f26434cfdab9d48f6e97a384
Marcin Rataj <m@lidel.org>
2010-05-31 02:29:52 +0200

    RESTful search: new slugify2 fix

--- search/urls.py
+++ search/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('mmda.search.views',
     (r'^$', 'create_search_result'),
-    url(r'^(?P<query_type>\S+)/(?P<query_string>\S+)/(?P<query_id>\w{40})/$', 'show_search_result', name='show-search-result'),
-    url(r'^(?P<query_type>\S+)/(?P<query_string>\S+)/$', 'create_search_result', name='create-search-result')
+    url(r'^(?P<query_type>\S+)/(?P<query_string>[^/]+)/(?P<query_id>\w{40})/$', 'show_search_result', name='show-search-result'),
+    url(r'^(?P<query_type>\S+)/(?P<query_string>[^/]+)/$', 'create_search_result', name='create-search-result')
     #url(r'^(?P<query_id>\w{40})/$', 'show_search_result', name='show-search-result-plain')
 )
