622bf5d44df8329c5ea8d10b535d845c68aaa12a
Marcin Rataj <m@lidel.org>
2011-01-01 16:43:19 +0100

    added generic keys.py file

---
+++ keys.py
@@ -0,0 +1,15 @@
+# keys, apis, and other stuff that mmda uses
+
+LASTFM_API_KEY = '00000000000000000000000000000000' # key that is used with last.fm database queries
+                                                    # if you use this software please replace it with your own key(!)
+                                                    # it can be obtained for free at http://www.last.fm/api/account
+
+FLICKR_API_KEY = '00000000000000000000000000000000' # flickr key
+                                                    # if you use this software please replace it with your own key(!)
+                                                    # it can be obtained for free at http://www.flickr.com/services/api
+
+                                                    # reCAPTCHA
+                                                    # you can get them at: https://admin.recaptcha.net/recaptcha/createsite/
+RECAPTCHA_PUB_KEY  = '0000000000000000000000000000000000000000'
+RECAPTCHA_PRIV_KEY = '0000000000000000000000000000000000000000'
+
